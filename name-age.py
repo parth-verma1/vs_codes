62e66579e7ef02a3f80f2483ff6b0beeac9f92f0
parth-verma1 <parth.verma140810@gmail.com>
2025-04-30 16:40:22 +0530

    Add files via upload

---
+++ name-age.py
@@ -0,0 +1,11 @@
+name = "Alex"
+age = 0
+if name == "Alex" or name == "John" and age >= 2:
+    print("hello! welcome")
+else:
+    print("good bye!")
+
+if name == "Alex" and age > 0 or age == 0:
+    print("hello there")
+else:
+    print("bye")
